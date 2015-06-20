0eea5db4103d51fe069e44d01283f421b1fceb84
Josh Samara <joshsamara@gmail.com>
2015-06-20 16:01:39 -0400

    Basic quiz page setup.

--- application.py
+++ application.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html')
 
+
+@app.route("/quiz", methods=['GET', 'POST'])
+def quiz():
+    if request.method == 'POST':
+        print request.form
+        return render_template('quiz.html')
+    else:
+        return render_template('quiz.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
