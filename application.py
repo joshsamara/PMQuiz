32c7172dba6e4458a34b5d7986cac19faf6359f9
Josh Samara <joshsamara@gmail.com>
2015-06-20 18:39:48 -0400

    Setup to handle tool choices.

--- application.py
+++ application.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 from flask import Flask, render_template, request
 from yaml import load
+from logic import choose_tools
 app = Flask(__name__)
 
 # Load tools from yaml
@@ -20,20 +21,23 @@ def home():
 @app.route("/quiz", methods=['GET', 'POST'])
 def quiz():
     if request.method == 'POST':
-        print request.form
-        return render_template('quiz.html')
+        tool_data, notes = choose_tools(request.form)
+        return results(True, tool_data, notes)
     else:
         return render_template('quiz.html')
 
 
 # Results Page
 @app.route("/results")
-def results(recommend=False, tool_data=None):
+def results(recommend=False, tool_data=None, notes=None):
     if tool_data is None:
         tool_data = tools
+    if notes is None:
+        notes = []
     return render_template('results.html',
                            recommend=recommend,
-                           tool_data=tool_data)
+                           tool_data=tool_data,
+                           notes=notes)
 
 
 if __name__ == "__main__":
---
+++ logic.py
@@ -0,0 +1,3 @@
+def choose_tools(form_data):
+    print form_data
+    return {}, []
